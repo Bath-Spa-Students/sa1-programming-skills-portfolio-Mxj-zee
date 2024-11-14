7765deb486d3cf9e721033a806acf1242e02c0be
Mxj-zee <wazeemansoor21@gmail.com>
2024-11-14 11:33:00 +0400

    Add files via upload

---
+++ Exercise - 3.py
@@ -0,0 +1,12 @@
+Details={"name": "wazeema","hometown": "india","age": 17}
+print(Details["name"])
+print(Details["hometown"])
+print(Details["age"])
+#advanced requirement
+name=input("enter your name:")
+hometown=input("enter your hometown:")
+age=input(" enter your age:")
+Details={"name": name,"hometown":hometown,"age": age}
+print(Details["name"])
+print(Details["hometown"])
+print(Details["age"])

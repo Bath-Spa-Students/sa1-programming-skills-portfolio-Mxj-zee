127ebabd2d02eb1086ca0c0c084f4040a3a7593d
Mxj-zee <wazeemansoor21@gmail.com>
2024-11-22 22:10:48 +0400

    Hello

---
+++ Exercise9/Exercise - 9.py
@@ -0,0 +1,8 @@
+def hey():
+    print("Hey")  # Fill in this blank to print "Hey" to the console
+
+def imp():
+    hey()  # Fill in this blank to call the hey() function
+
+if __name__ == "__imp__":
+    imp()
